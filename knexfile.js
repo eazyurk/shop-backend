91ac0954553938b60f062dd77aa6b94dd0dc3943
Jan-Willem de Boer <janwillem.urk@gmail.com>
2020-09-25 00:21:14 +0200

    Docker setup

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,7 @@ module.exports = {
       client: 'mysql',
       connection: {
         host : process.env.MYSQL_HOST,
+        port : process.env.MYSQL_PORT,
         user : process.env.MYSQL_USERNAME,
         password : process.env.MYSQL_PASSWORD,
         database : process.env.MYSQL_DATABASE,
